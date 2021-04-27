bc04b3c36c28534d2ddfbac39cb5caef99c2e08c
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-04-27 11:04:57 +0100

    Fix rubocop errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-    def display_navbar
-        if logged_in?
-            render "shared/logged_in_navbar"
-        else
-            render "shared/log_in"
-        end
+  def display_navbar
+    if logged_in?
+      render 'shared/logged_in_navbar'
+    else
+      render 'shared/log_in'
     end
+  end
 end
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,9 +1,9 @@
 module TransactionsHelper
-    def transaction_group
-        if @transactions
-            render "shared/image"
-        else
-            render "shared/small_text"
-        end
+  def transaction_group
+    if @transactions
+      render 'shared/image'
+    else
+      render 'shared/small_text'
     end
+  end
 end
